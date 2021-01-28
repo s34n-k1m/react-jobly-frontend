f08d9c4edaf2356eb16d28037799030adc78b7c3
@jonathanlei <jonathanlei98@gmail.com>
2021-01-27 19:59:50 -0500

    trying to debug isloading

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -10,30 +10,54 @@ App -> Routes -> CompanyList -> CompanyCard
 */
 function CompanyList() {
   const [companies, setCompanies] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
 
   /* get companies */
   useEffect(function getCompanies() {
     async function getCompaniesWithApi() {
+      console.log(companies, "companies on init");
       const resCompanies = await JoblyApi.getCompanies();
+      console.log(resCompanies, "res Companies");
       setCompanies(resCompanies);
     }
     getCompaniesWithApi();
+    setIsLoading(false);
   }, []);
 
-  /* Make API request to get filtered companies list by name*/
-  async function searchCompanyByName(name) {
-    setCompanies(null);
-    const resCompanies = await JoblyApi.getCompanies(name);
-    setCompanies(resCompanies);
+  // /* Make API request to get filtered companies list by name*/
+  // async function searchCompanyByName(name) {
+  //   setCompanies(null);
+  //   const resCompanies = await JoblyApi.getCompanies(name);
+  //   setCompanies(resCompanies);
+  // }
+
+
+  function searchCompany() {
+    setIsSearching(true);
   }
 
-  // search might return [], could add loading state
-  if (companies === null) return <div>Loading...</div>;
+  useEffect(function search() {
+    async function searchCompanyByName(name) {
+      console.log(companies, "companies on search");
+      const resCompanies = await JoblyApi.getCompanies(name);
+      setCompanies(resCompanies);
+    }
+    if (isSearching){
+      console.log(companies, "companies before search");
+      setIsLoading(true);
+      searchCompanyByName();
+      console.log(companies, "companies after search");
+      setIsLoading(false);
+    }
+  }, [isSearching]);
 
-  if (companies.length === 0) {
+
+  if (isLoading) return <div>Loading...</div>;
+  if (companies !== null && companies.length === 0) {
     return (
       <div>
-        <SearchForm search={searchCompanyByName} />
+        <SearchForm search={searchCompany} />
         No results found.
       </div>);
   }
@@ -42,7 +66,7 @@ function CompanyList() {
   // add keys
   return (
     <div className="CompanyList">
-      <SearchForm search={searchCompanyByName} />
+      <SearchForm search={searchCompany} />
       {companies.map(c => <CompanyCard key={c.handle} company={c} />)}
     </div>);
 }
